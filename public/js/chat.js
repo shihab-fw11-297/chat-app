97dc4d760058799e918fb2f108dab25ea778f2fd
shihab shaikh <123071760+ShihabLahagora@users.noreply.github.com>
2023-12-11 20:59:24 +0530

    add css,mustache

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,55 +2,71 @@ let socket = io();
 
 
 socket.on('connection', () => {
-    console.log("A new user just connected")
+  console.log("A new user just connected")
 });
 
 socket.on('disconnect', () => {
-    console.log("A new user just dis-connected");
+  console.log("A new user just dis-connected");
 });
 
 socket.on('newMessage', function (message) {
-    console.log(message)
-    let li = document.createElement(`li`);
-    li.innerText = `${message.from} : ${message.text}`
-    document.querySelector('body').append(li);
+  const formattedTime = moment(message.createdAt).format('LT');
+  const template = document.querySelector('#message-template').innerHTML;
+  const html = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
+
+  const div = document.createElement('div');
+  div.innerHTML = html
+
+  document.querySelector('#messages').appendChild(div);
 });
 
 socket.on('newLocationMessage', function (message) {
-    console.log(`newLocationMessage`,message)
-    let li = document.createElement(`li`);
-    let a = document.createElement(`a`);
-    a.setAttribute('target','_blank')
-    a.setAttribute('href',message.url);
-    a.innerText = a
-    document.querySelector('body').append(a);
+  const formattedTime = moment(message.createdAt).format('LT');
+  console.log("newLocationMessage", message);
+
+  const template = document.querySelector('#location-message-template').innerHTML;
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
+
+  const div = document.createElement('div');
+  div.innerHTML = html
+
+  document.querySelector('#messages').appendChild(div);
+
 });
 
-document.querySelector('#submit-btn').addEventListener('click', function(e) {
-    e.preventDefault();
-  
-    socket.emit("createMessage", {
-      from:"User",
-      text: document.querySelector('input[name="message"]').value
-    }, function() {
-     
+document.querySelector('#submit-btn').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  socket.emit("createMessage", {
+    from: "User",
+    text: document.querySelector('input[name="message"]').value
+  }, function () {
+
+  })
+})
+
+
+document.querySelector('#send-location').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.')
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
     })
-  })    
-
-
-  document.querySelector('#send-location').addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if (!navigator.geolocation) {
-      return alert('Geolocation is not supported by your browser.')
-    }
-  
-    navigator.geolocation.getCurrentPosition(function(position) {
-      socket.emit('createLocationMessage', {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      })
-    }, function() {
-      alert('Unable to fetch location.')
-    })
-  });
+  }, function () {
+    alert('Unable to fetch location.')
+  })
+});
